40ddf052b0586cae56bbff89fc9f22527ad4114f
孙飞 <sun1195255889@163.com>
2017-12-01 17:09:32 +0800

    /vue-particles build error

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -16,29 +16,34 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    publicPath:
+      process.env.NODE_ENV === 'production'
+        ? config.build.assetsPublicPath
+        : config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      vue$: 'vue/dist/vue.esm.js',
+      '@': resolve('src')
     }
   },
   module: {
     rules: [
-      ...(config.dev.useEslint? [{
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
-        include: [resolve('src'), resolve('test')],
-        options: {
-          formatter: require('eslint-friendly-formatter'),
-          emitWarning: !config.dev.showEslintErrorsInOverlay
-        }
-      }] : []),
+      ...(config.dev.useEslint
+        ? [
+            {
+              test: /\.(js|vue)$/,
+              loader: 'eslint-loader',
+              enforce: 'pre',
+              include: [resolve('src'), resolve('test')],
+              options: {
+                formatter: require('eslint-friendly-formatter'),
+                emitWarning: !config.dev.showEslintErrorsInOverlay
+              }
+            }
+          ]
+        : []),
       {
         test: /\.vue$/,
         loader: 'vue-loader',
@@ -47,7 +52,11 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [
+          resolve('src'),
+          resolve('test'),
+          resolve('node_modules/vue-particles')
+        ]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -75,4 +84,4 @@ module.exports = {
       }
     ]
   }
-}
+};
