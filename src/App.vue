570e6ac341146fded4866a6bb0675b122d38a80f
孙飞 <sun1195255889@163.com>
2017-12-01 15:11:34 +0800

    first init

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app">
+    <header>
+        <h1>欢迎来到VueJs</h1>
+      <vue-particles  color="#dedede"></vue-particles>
+
+    </header>
+    <main>
+       <router-view/>
+    </main>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#app header {
+  height: 45vh;
+  background-color: #00bcd4;
+  color: blue;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(#0097a7, #00bcd4);
+  background-size: 250% 250%;
+  animation: kKvztC 10s ease infinite;
+  font-weight: 400;
+  transition: height 250ms ease-in-out;
+  user-select: none;
+}
+#app header h1 {
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  font-weight: 700;
+  align-items: center;
+  transition: font-size 250ms ease-in-out, padding 150ms ease-in;
+  background-color: #002635;
+  color: white;
+  padding: 0.5rem 1rem;
+  margin: 0;
+  width: auto;
+  user-select: text;
+}
+@media only screen and (min-width: 375px) {
+  #app header h1 {
+    font-size: 2rem;
+  }
+}
+@media only screen and (min-width: 768px) {
+  #app header h1 {
+    font-size: 3.5rem;
+    padding: 1rem 2rem;
+  }
+}
+#app main {
+  position: absolute;
+  top: 40vh;
+  right: 0;
+  left: 0;
+  margin: 0 auto;
+  max-width: 840px;
+  z-index: 2;
+}
+#particles-js{
+  position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width:100%;
+    height:100%;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <section class="section" v-for="item in items">
+      <span class="tool-bar">{{moment(item.created_at).format('YYYY-MM-DD')}}</span>
+      <div class="section-content">
+          <hr class="split-bar">
+          <h1 class="content-title">{{item.name}}</h1>
+          <hr class="split-bar">
+      </div>
+      <a class="read-more" target="_blank" v-bind:href="item.clone_url">READ MORE</a>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  created () {
+    axios.get('https://api.github.com/users/anhuisunfei/repos?sort=created&order=desc')
+      .then(res => {
+        this.items = res.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  },
+  data () {
+    return {
+      items: []
+    }
+  }
+}
+</script>
+
+<style>
+.section {
+  position: relative;
+  width: 100%;
+  background-color: white;
+  box-shadow: 0 0 4px 1px rgba(0, 0, 0, 0.2);
+  background-color: white;
+  outline: 1px solid rgba(0, 0, 0, 0.125);
+  box-shadow: 0 0 4px 1px rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 1.5rem;
+  :last-child {
+    border-bottom-width: 0;
+  }
+}
+.tool-bar {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  width: 100%;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  z-index: 1;
+  background: hsl(166, 90%, 20%);
+  color: white;
+  position: relative;
+  -webkit-transition: background-color 125ms ease-in-out;
+  transition: background-color 125ms ease-in-out;
+  padding-top: 1.125rem;
+  padding-bottom: 1.125rem;
+}
+@media only screen and (min-width: 768px) {
+  .section {
+    margin-bottom: 3rem;
+    padding-bottom: 3rem;
+  }
+}
+.section-content {
+  max-width: 100%;
+  padding: 1.125rem 1.5rem;
+}
+@media only screen and (min-width: 768px) {
+  padding: 1.5rem 3rem;
+  padding-top: 1.5rem;
+}
+.split-bar {
+  border: 0;
+  width: 75%;
+  margin: 0.75rem auto;
+  border-bottom: 1px solid #eee;
+}
+
+.content-title {
+  font-size: 1.25rem;
+  line-height: 1.5rem;
+  text-align: center;
+  margin-bottom: 0;
+}
+@media only screen and (min-width: 768px) {
+  .content-title {
+    font-size: 1.5rem;
+    line-height: 2.25rem;
+  }
+}
+
+
+
+.read-more {
+  display: inline-block;
+  background-color: white;
+  padding: 0.5rem 1.5rem;
+  font-size: 1.125rem;
+  font-weight: bold;
+  text-decoration: none;
+  text-align: center;
+  text-transform: uppercase;
+  border-width: 2px;
+  border-style: solid;
+  -webkit-transition: all 125ms ease-in-out;
+  transition: all 125ms ease-in-out;
+  font-family: sans-serif;
+  background-color: hsl(166, 90%, 20%);
+  color: white;
+  border-color: hsl(166, 90%, 20%);
+}
+</style>
+
+
+
